538d671e3815d7a91959a3973316bbf93195f7fe
Célestin Piaton <celestin.piatonpro@gmail.com>
2026-02-17 21:53:34 +0100

    Fix overflow mobile header prod

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   
   return (
-    <section className="relative h-dvh md:h-[90vh] w-full overflow-hidden">
+    <section className="relative h-[90vh] w-full">
       <Image
               src="/images/bg-billeterie.jpg"
               alt="Festival Technosaure"
@@ -21,10 +21,8 @@ export default function Header() {
               className="z-0 object-center object-cover"
             />
       <div className="absolute inset-0 bg-black opacity-30"></div>
-      <div className="absolute top-0 left-0 w-full z-20">
         <BannerTop />
         <Navbar onToggle={() => setIsMenuOpen(prev => !prev)} />
-      </div>
       <MobileMenu isOpen={isMenuOpen} onClose={() => setIsMenuOpen(false)} />
       <HeaderContent />
     </section>
